62313ccff7cd3fc9c499b26199a7eb91e74ff76e
robertHer <totolero769@gmail.com>
2018-06-15 12:15:32 -0700

    parent to child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentComponentComponent } from './parent-component/parent-component.component';
+import { ChildcomponentComponent } from './childcomponent/childcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponentComponent,
+    ChildcomponentComponent
   ],
   imports: [
     BrowserModule
